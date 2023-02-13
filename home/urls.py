03adadbddcb1b106e2dc57b66c618e1dc2f55681
AniketMunde <aniketmunde29thjan@gmail.com>
2023-02-13 11:30:03 +0530

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,30 @@
+from xml.etree.ElementInclude import include
+from django import views
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('bookings' , views.bookings, name='bookings'),
+    path('doctors' , views.doctors, name='doctors'),
+    path('contact' , views.contact, name='contact'),
+    path('department' , views.department, name='departments'),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+    path("profile", views.profile, name= "profile"),
+    path("viewbookings", views.viewbookings, name="viewbookings"),
+    path('adddoctors' , views.adddoctors, name='adddoctors'),
+    path('delete_doctor/<int:pid>',views.Delete_Doctor,name='delete_doctor')
+    
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
